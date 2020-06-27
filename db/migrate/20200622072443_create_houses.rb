e4777139572a190702855d694f24842695f2731d
shamseldeen <shamseldeen314@gmail.com>
2020-06-27 12:55:24 +0200

    remove null

--- db/migrate/20200622072443_create_houses.rb
+++ db/migrate/20200622072443_create_houses.rb
@@ -2,8 +2,8 @@ class CreateHouses < ActiveRecord::Migration[6.0]
   def change
     create_table :houses do |t|
       t.string :house_name, {null: false, limit: 40} 
-      t.integer :electricity, {null: true, limit: 1} 
-      t.integer :water, {null: true, limit: 1}
+      t.integer :electricity, {limit: 1} 
+      t.integer :water, {limit: 1}
       t.timestamps
     end
   end
